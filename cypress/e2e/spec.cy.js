f85448bac99fb2792ffd8ed9729696924ed018a8
Griffito <eurocracy2@gmail.com>
2023-10-24 15:40:00 +0100

    Add example cypress test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/')
+describe('Example Test', () => {
+  it('outputs equilateral message', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('[data-testid="x-field"]').type('5');
+    cy.get('[data-testid="y-field"]').type('5');
+    cy.get('[data-testid="z-field"]').type('5');
+
+    cy.get('[data-testid="submit-btn"]').click();
+
+    cy.get('[data-testid="result-title"]').should('contain', 'Equilateral');
   })
 })
