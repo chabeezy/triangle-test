a2be77720a19a68455f2324d24e4e164ac60732d
Griffito <eurocracy2@gmail.com>
2023-10-24 16:23:25 +0100

    Added Selenium test

--- selenium/src/main/java/org/example/Main.java
+++ selenium/src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -17,6 +19,16 @@ public class Main {
         driver.getTitle();
 
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        driver.findElement(By.xpath("//*[@data-testid='x-field']")).sendKeys("5");
+        driver.findElement(By.xpath("//*[@data-testid='y-field']")).sendKeys("5");
+        driver.findElement(By.xpath("//*[@data-testid='z-field']")).sendKeys("5");
+
+        driver.findElement(By.xpath("//*[@data-testid='submit-btn']")).click();
+
+        String result = driver.findElement(By.xpath("//*[@data-testid='result-title']")).getText();
+
+        Assert.assertTrue(result.contains("Equilateral"));
 
         driver.quit();
     }
